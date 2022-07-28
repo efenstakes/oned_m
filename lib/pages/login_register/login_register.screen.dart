4ade6e2ac4754e8b619b5fbd0b60fd0e612dc42b
efenstakes <efenstakes101@gmail.com>
2022-07-28 18:44:28 +0300

    update page layout to add scaffold

--- lib/pages/login_register/login_register.screen.dart
+++ lib/pages/login_register/login_register.screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:oned_m/pages/login_register/create_account.screen.dart';
 import 'package:oned_m/pages/login_register/login.screen.dart';
 
@@ -11,16 +12,19 @@ class LoginRegisterScreen extends StatefulWidget {
 }
 
 class _LoginRegisterScreenState extends State<LoginRegisterScreen> {
-
   bool _isInLogging = true;
-
 
 
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: _isInLogging ? LoginScreen(switchPage: switchPage,) : CreateAccountScreen(switchPage: switchPage,),
+    Size screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        margin: EdgeInsets.only(top: screenSize.width/5),
+        child: _isInLogging ? LoginScreen(switchPage: switchPage,) : CreateAccountScreen(switchPage: switchPage,),
+      ),
     );
   }
 
