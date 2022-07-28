91efd3583199cc45a1eb0c325d56a39abdfc1419
efenstakes <efenstakes101@gmail.com>
2022-07-28 20:00:07 +0300

    add error handling

--- lib/pages/login_register/login.screen.dart
+++ lib/pages/login_register/login.screen.dart
@@ -23,6 +23,8 @@ class _LoginScreenState extends State<LoginScreen> {
 
   bool _isLoading = false;
 
+  String _error = "";
+
 
   @override
   Widget build(BuildContext context) {
@@ -91,36 +93,29 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
               const SizedBox(height: 40),
 
+              (_error == "" )
+                ? Container()
+                : 
+                  Container(
+                    margin: const EdgeInsets.all(10),
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.yellow[200],
+                      borderRadius: const BorderRadius.all(Radius.circular(10)),
+                    ),
+                    child: Text(_error),
+                  ),
 
               // add button
               FloatingActionButton.extended(
                 elevation: 0,
                 focusElevation: 0,
                 hoverElevation: 0,
-                onPressed: (){
-                  _formKey.currentState!.save();
-
-                  if( !_formKey.currentState!.validate() ) {
-                    print("form error ");
-                    return;
-                  }
-
-                  setState(()=> _isLoading = true);
-
-                  print("form can submit");
-
-                  try {
-                    FirebaseAuth.instance.signInWithEmailAndPassword(
-                      email: _emailInputController.text.trim(), 
-                      password: _passwordInputController.text.trim()
-                    );
-                  } catch(e) {
-                    print("could not login ${e.toString()}");
-                  }
-
-                  setState(()=> _isLoading = true);
-                }, 
-                label: Text("Login"),
+                onPressed: _login, 
+                label: Text(
+                  _isLoading ? "Logging in.." : "Login"
+                ),
+                icon: _isLoading ? CircularProgressIndicator() : null,
               ),
               const SizedBox(height: 20),
               TextButton(onPressed: ()=> widget.switchPage(), child: Text("No Account? Create One"),),
@@ -132,5 +127,54 @@ class _LoginScreenState extends State<LoginScreen> {
         ),
       ),
     );
+  }
+
+
+  _login() async {
+    _formKey.currentState!.save();
+
+    if( !_formKey.currentState!.validate() ) {
+      print("form error ");
+      return;
+    }
+
+    setState(() {
+      _isLoading = true;
+      _error = "";
+    });
+
+    print("form can submit");
+
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: _emailInputController.text.trim(), 
+        password: _passwordInputController.text.trim()
+      );
+    } on FirebaseAuthException catch (e) {
+      print("\n\n\n");
+      print("could not login ${e.code}");
+      print("\n\n\n");
+      switch (e.code) {
+        case "invalid-email":
+          setState(()=> _error = "Check email and password");
+          break;
+
+        case "user-disabled":
+          setState(()=> _error = "Check email and password");
+          break;
+          
+        case "user-not-found":
+          setState(()=> _error = "Check email and password");
+          break;
+          
+        case "wrong-password":
+          setState(()=> _error = "Check email and password");
+          break;
+
+        default:
+      }
+    }
+
+    setState(()=> _isLoading = true);
   }
 }
