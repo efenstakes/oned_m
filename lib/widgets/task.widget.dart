3011b52e3e13ea0891d09206ee660be9dad931ba
efenstakes <efenstakes101@gmail.com>
2022-07-29 17:07:38 +0300

    add progress logic and routing logic

--- lib/widgets/task.widget.dart
+++ lib/widgets/task.widget.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:jiffy/jiffy.dart';
 import 'package:oned_m/models/task.model.dart';
+import 'package:oned_m/pages/login_register/login_register.screen.dart';
+import 'package:oned_m/pages/task_details/task_details.screen.dart';
+import 'package:oned_m/pages/task_ongoing/task_ongoing.screen.dart';
 
 
 class TaskWidget extends StatefulWidget {
@@ -20,96 +23,97 @@ class _TaskWidgetState extends State<TaskWidget> {
   Widget build(BuildContext context) {
     Color? color = Colors.green[900];
 
-    if( widget.task.progress > 0 ) {
+    if( widget.task.progress > 0 && widget.task.progress < 100 ) {
       color = Colors.blue[400];
     } 
     if( widget.task.progress != 100 && Jiffy(widget.task.deadline).isBefore(Jiffy()) ) {
       color = Colors.orange[300];
     }
 
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10),
+    return InkWell(
+      onTap: ()=> Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (ctxt)=> TaskDetailsScreen(task: widget.task,)
+          // builder: (ctxt)=> TaskOngoingScreen(task: widget.task,)
+        )
       ),
-      child: Container(
-        padding: const EdgeInsets.all(8),
-        constraints: const BoxConstraints(
-          maxHeight: 200,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // title and done status
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  widget.task.title!,
-                  style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                        fontWeight: FontWeight.bold,
-                      ),
-                ),
-                Container(
-                  width: 12,
-                  height: 12,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: color,
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          constraints: const BoxConstraints(
+            maxHeight: 200,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // title and done status
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    widget.task.title!,
+                    style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                          fontWeight: FontWeight.bold,
+                        ),
                   ),
-                )
-              ],
-            ),
+                  Container(
+                    width: 12,
+                    height: 12,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: color,
+                    ),
+                  )
+                ],
+              ),
 
-            // description
-            Text(
-              widget.task.description!,
-              style: Theme.of(context).textTheme.bodyMedium,
-              overflow: TextOverflow.ellipsis,
-              maxLines: 2,
-            ),
+              // description
+              Text(
+                widget.task.description!,
+                style: Theme.of(context).textTheme.bodyMedium,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
 
-            const Spacer(),
+              const Spacer(),
 
-            // actions
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                FloatingActionButton(
-                  onPressed: ()=> widget.onDelete(),
-                  child: const Icon(Icons.delete_outline_outlined),
-                  mini: true,
-                  elevation: 0,
-                  key: Key("delete"),
-                  backgroundColor: Colors.red[600],
-                ),
-                FloatingActionButton(
-                  onPressed: () {},
-                  child: const Icon(Icons.edit_attributes_outlined),
-                  mini: true,
-                  elevation: 0,
-                  key: Key("edit"),
-                  backgroundColor: Colors.black54,
-                ),
-                FloatingActionButton(
-                  onPressed: ()=> widget.onProgress(),
-                  child: const Icon(Icons.query_stats),
-                  mini: true,
-                  elevation: 0,
-                  key: Key("set status"),
-                  backgroundColor: Colors.greenAccent[800],
-                ),
-                FloatingActionButton(
-                  onPressed: ()=> widget.onDone(),
-                  child: const Icon(Icons.done_all),
-                  mini: true,
-                  elevation: 0,
-                  key: Key("done"),
-                  backgroundColor: Colors.green[800],
-                ),
-              ],
-            )
-          ],
+              // actions
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  FloatingActionButton(
+                    onPressed: ()=> widget.onDelete(),
+                    child: const Icon(Icons.delete_outline_outlined),
+                    mini: true,
+                    elevation: 0,
+                    key: Key("delete"),
+                    backgroundColor: Colors.red[600],
+                  ),
+                  FloatingActionButton(
+                    onPressed: ()=> widget.onProgress(),
+                    child: const Icon(Icons.stacked_bar_chart_rounded),
+                    mini: true,
+                    elevation: 0,
+                    key: Key("set status"),
+                    backgroundColor: Colors.greenAccent[800],
+                  ),
+                  FloatingActionButton(
+                    onPressed: ()=> widget.onDone(),
+                    child: const Icon(Icons.done_all),
+                    mini: true,
+                    elevation: 0,
+                    key: Key("done"),
+                    backgroundColor: Colors.green[800],
+                  ),
+                ],
+              ),
+
+            ],
+          ),
         ),
       ),
     );
