e1c9cd2a29d69ff83885c5f5f1a1a07a6658d673
efenstakes <efenstakes101@gmail.com>
2022-07-30 21:04:26 +0300

    add quote model

---
+++ lib/models/quote.model.dart
@@ -0,0 +1,15 @@
+
+
+class Quote {
+  String text;
+  String author;
+
+  Quote({ required this.text, required this.author });
+
+  static Quote fromMap(Map<String, dynamic> map) {
+    return Quote(
+      text: map['text'], 
+      author: map['author'],
+    );
+  }
+}
