20413e516373058aa5aafca871435bf103b710b3
efenstakes <efenstakes101@gmail.com>
2022-08-07 12:38:17 +0300

    add selectable chip widget

---
+++ lib/widgets/selectable_chip.widget.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+
+class SelectableChipWidget extends StatelessWidget {
+  String text;
+  bool isSelected;
+  Function onSelect;
+
+  SelectableChipWidget({
+    Key? key,
+    required this.text,
+    required this.isSelected,
+    required this.onSelect,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: ()=> onSelect(),
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 600),
+        child: Text(
+          text,
+          style: TextStyle(
+            color: isSelected ? Colors.white : Colors.black87,
+          ),
+        ),
+        padding: const EdgeInsets.symmetric(
+          vertical: 9,
+          horizontal: 16,
+        ),
+        decoration: BoxDecoration(
+          color: isSelected ? Colors.black87 : Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+            color: isSelected ? Colors.transparent : Colors.black87,
+            width: 1,
+          )
+        ),
+      ),
+    );
+  }
+}
