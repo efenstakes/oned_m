88bb6179a1d297e2f0de9c3282d25d588f288efb
efenstakes <efenstakes101@gmail.com>
2022-08-07 12:05:59 +0300

    add a no tasks widget

--- lib/pages/home/home.screen.dart
+++ lib/pages/home/home.screen.dart
@@ -11,6 +11,7 @@ import 'package:oned_m/pages/home/progress_picker.dart';
 import 'package:oned_m/pages/home/quote_of_the_day.widget.dart';
 import 'package:oned_m/pages/login_register/login_register.screen.dart';
 import 'package:oned_m/pages/task_ongoing/task_ongoing.screen.dart';
+import 'package:oned_m/widgets/no_tasks.widget.dart';
 import 'package:oned_m/widgets/stat_card.widget.dart';
 import 'package:oned_m/widgets/stats.widget.dart';
 import 'package:oned_m/widgets/task.widget.dart';
@@ -146,49 +147,17 @@ class _HomeScreenState extends State<HomeScreen> {
 
             ( !_isLoadingTasks && _tasks.isEmpty ) 
               ?
-                Container(
-                  margin: const EdgeInsets.symmetric(
-                    vertical: 120
-                  ),
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 20, horizontal: 10,
-                  ),
-                  decoration: BoxDecoration(
-                    color: Colors.brown[200],
-                    borderRadius: const BorderRadius.all(Radius.circular(10)),
-                  ),
-                  child: Column(
-                    children: [
-
-                      Text(
-                        "No Tasks", 
-                        style: Theme.of(context).textTheme.headline5!.copyWith(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text("Click the add button to add a task", style: TextStyle(), textAlign: TextAlign.center,),
-                      SizedBox(height: 20),
-
-                      FloatingActionButton.extended(
-                        elevation: 0,
-                        focusElevation: 0,
-                        hoverElevation: 0,
-                        onPressed: () {
-                          showModalBottomSheet(
-                            isScrollControlled: true,
-                            isDismissible: true,
-                            context: context, 
-                            builder: (context) {
-                              return const AddTaskScreen();
-                            },
-                          );
-                        }, 
-                        label: const Text("Add Task"),
-                        icon: const Icon(Icons.add),
-                        key: const Key("HP:Add Task"),
-                      ),
-                    ],
-                  ),
+                NoTasksWidget(
+                  addTask: () {
+                    showModalBottomSheet(
+                      isScrollControlled: true,
+                      isDismissible: true,
+                      context: context, 
+                      builder: (context) {
+                        return const AddTaskScreen();
+                      },
+                    );
+                  }
                 )
               : Container(),
 
