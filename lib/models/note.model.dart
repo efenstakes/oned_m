163133d99fcf7c792d0f09045be0f90c5a3b1756
efenstakes <efenstakes101@gmail.com>
2022-08-11 08:55:41 +0300

    add note model

---
+++ lib/models/note.model.dart
@@ -0,0 +1,24 @@
+
+
+
+class Note {
+  String? id;
+  String? title;
+  String? text;
+
+  Note({ 
+    this.id,
+    this.title,
+    this.text,
+  });
+
+
+  static Note fromMap(Map<String, dynamic> map) {
+    return Note(
+      id: map['id'],
+      title: map['title'],
+      text: map['text'], 
+    );
+  }
+
+}
